700b4f770a987b0293eea22acc3cacb23ce53bf6
Nicolas bobadilla sanchez <nicolas.bobadilla.s@mail.pucv.cl>
2022-05-07 20:40:01 -0400

    falta crear el scss

--- js/app.js
+++ js/app.js
@@ -22,129 +22,107 @@ form.addEventListener('submit',function(e){
 })
 */
 
-
-const comenta=document.getElementById('comentarios');
-const comens=document.getElementById('comentar');
-
+const comentarios=document.getElementById('comentar');
+const botones=document.getElementById('btn_group1');
+const comens=document.getElementById('ing_comen');
+const templa=document.getElementById('comen_1').content;
+const frag=document.createDocumentFragment();
+var botones_disable = true;
 var i=0;
-var j=1;
-comenta.addEventListener('submit',function(e){
-    e.preventDefault();
 
-    var nombr= document.getElementById('usuario').value;
-    var mensa= document.getElementById('mensaje').value;
-    comenta.reset();
-const a_ele = document.createElement('a');
-const div_ele = document.createElement('div');
-const div2_ele = document.createElement('div');
-const div3_ele = document.createElement('div');
-const bot_ele = document.createElement('button');
-const h5_ele=document.createElement('h5');
-const p_ele=document.createElement('p');
-a_ele.ariaCurrent=true;
-if((i%2)==0){
-    a_ele.id='a'+j;
-    a_ele.classList.add('list-group-item','list-group-item-action');
-    i++;
-    console.log('Entro el el if')
-}
-else{
-    a_ele.id='a'+j;
-    a_ele.classList.add('list-group-item','list-group-item-action','active');
-    i++;
-}
-    bot_ele.classList.add('btn','btn-danger','me-md-2');
-    bot_ele.id='b_cerrar';
-    bot_ele.innerText='X';
-    bot_ele.disabled=true;
-    bot_ele.value='a'+j;
-    bot_ele.type='button';
-    bot_ele.setAttribute("click","");
-    console.log(bot_ele.value,'nuevo boton id');
-    div_ele.classList.add('row','d-flex','w-100','justify-content-md-between');
-    div2_ele.classList.add('col-4');
-    div3_ele.classList.add('col-1');
-    h5_ele.classList.add('mb-1');
-    p_ele.classList.add('mb-1','text-break');
-    h5_ele.textContent=nombr;
-    p_ele.textContent=mensa;
-
-    div3_ele.appendChild(bot_ele);
-    div2_ele.appendChild(h5_ele);
-    div_ele.appendChild(div2_ele);
-    div_ele.appendChild(div3_ele);
+botones.addEventListener('click',event =>{
+   event.preventDefault();
    
-    j++;
-    a_ele.appendChild(div_ele);
-    a_ele.appendChild(p_ele);
-    comens.appendChild(a_ele);
+    var name=comens.querySelector('#usuario').value;
+    var clave=comens.querySelector('#pass').value;
+    var men=comens.querySelector('#mensaje').value;
+  
+   if(event.target.id=='b_enviar')
+   {    console.log('1');
+
+       templa.querySelector('h5').textContent=name;
+       templa.querySelector('p').textContent=men;
+       templa.querySelector('#b_cerrar').value='a'+i;
+       
+        if((i%2)==0){
+            templa.querySelector('a').id='a'+i;
+            templa.querySelector('a').classList.add('bg-dark','text-light');
+            i++; 
+        }
+        else{
+            templa.querySelector('a').id='a'+i;
+            templa.querySelector('a').classList.remove('bg-dark','text-light');
+            
+            i++;
+        }
+        if(botones_disable==false)
+        {
+            templa.querySelector('#b_cerrar').disabled=false;
+        }
+        else if(botones_disable==true)
+        {
+            templa.querySelector('#b_cerrar').disabled=true;
+        }
+
+        let clone=document.importNode(templa,true);
+        frag.appendChild(clone);
+        comentarios.appendChild(frag);
+
+    }
+    
+    else if(event.target.id=='b_elim')
+    {       
+            var botones_int=document.querySelectorAll("#b_cerrar",".boton");
+          
+            var z=0;
+            if(botones_disable){
+                botones_int.forEach(function(){
+                    botones_int[z].disabled=false;
+                    z++;
+                   
+                });
+                botones_disable = false;
+            }
+            else{
+                botones_int.forEach(function(){
+                    botones_int[z].disabled=true;
+                    z++;
+                });
+                botones_disable = true;
+                }
+        }
+
+     else if(event.target.id=='b_com')   
+     {
+                   
+        if((name=='admin')&(clave=='admin')){
+            botones.querySelector('#b_elim').classList.remove('invisible');
+            
+        }
+        else if((name=='admin')&(clave=='admin1'))
+        {
+            botones.querySelector('#b_elim').classList.add('invisible');
+
+            var botones_int=document.querySelectorAll("#b_cerrar",".boton");
+            var z=0;
+            console.log(botones_int);
+            botones_int.forEach(function(){
+                botones_int[z].disabled=true;
+                z++;
+               
+            });
+        }
+
+     }
 })
 
-var botones_disable = true;
-document.querySelector("#eliminar_c").addEventListener("click",function(e){
-e.preventDefault();
-var botones=document.querySelectorAll("#b_cerrar",".boton");
-var i=0;
-if(botones_disable){
-    botones.forEach(function(){
-        botones[i].disabled=false;
-        i++;
-        botones_disable = false;
-    });
-}
-else{
-    botones.forEach(function(){
-        botones[i].disabled=true;
-        i++;
-    });
-    botones_disable = true;
-}
-});
-
-
-let contenedor_a = document.querySelector("#comentar")
-        var value_boton;
-        var id_bot;
-        contenedor_a.addEventListener("click", (event) => {
-            value_boton= `${event.target.value}`;
-            id_bot= `${event.target.id}`;
-
-        })
-contenedor_a.addEventListener("click",function (e){
-    e.preventDefault();
-           if(id_bot=='b_cerrar'){
-                comens.removeChild(document.getElementById(value_boton));
+comentarios.addEventListener("click",event =>{
+    event.preventDefault();
+           if(event.target.id=='b_cerrar'){
+               console.log(event.target.value);
+            comentarios.removeChild(document.getElementById(event.target.value));
            }
        
 });
-
-let  form_com= document.querySelector("#usuarios_com");
-
-    form_com.addEventListener("click",function (e){
-        e.preventDefault();
-        var user= document.getElementById('user').value;
-        var pass= document.getElementById('pass').value;
-        var id_bot;   
-        var boton=document.querySelector("#eliminar_c",".boton");
-
-
-        form_com.addEventListener("click", (event) => {
-            id_bot= `${event.target.id}`;
-        })
-
-
-            if((user=='admin')&(pass=='admin')){
-                
-                boton.disabled=false;
-                form_com.reset();
-            }
-            else if((user=='admin')&(pass=='cerrar')&(id_bot=='des_boton'))
-            {
-                boton.disabled=true;
-             
-                form_com.reset();
-            }
-        
-    });
 
 
