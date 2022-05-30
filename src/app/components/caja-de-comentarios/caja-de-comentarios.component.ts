b1cfe1e4ca189f821c5cff15fb3c8a0148ff215c
Nicolas bobadilla sanchez <nicolas.bobadilla.s@mail.pucv.cl>
2022-05-30 14:22:56 -0400

    caja de comentarios puesta

--- src/app/components/caja-de-comentarios/caja-de-comentarios.component.ts
+++ src/app/components/caja-de-comentarios/caja-de-comentarios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var funcion_en:any;
 @Component({
   selector: 'app-caja-de-comentarios',
   templateUrl: './caja-de-comentarios.component.html',
@@ -9,7 +9,16 @@ export class CajaDeComentariosComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  j=0;
+  boton=true;  
+    ngOnInit(): void {
+    }
+    funcion1(number:number,name:string ,men:string,cla:string){
+        funcion_en(number,name,men,cla,this.j,this.boton);
+        if(number==3){
+          if(this.boton==true){this.boton=false} else{this.boton=true}
+      }
+        this.j++; 
+        
+    }
 }
